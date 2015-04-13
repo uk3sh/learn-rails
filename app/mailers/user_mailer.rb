b41a9293f07dd01478c38420c3076c9a0dc3cc42
Ukesh Kumar <godsfear@gmail.com>
2015-04-13 09:59:03 +0530

    Added Email feature

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,6 @@
+class UserMailer < ApplicationMailer
+	def contact_email(contact)
+		@contact = contact
+		mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
+	end
+end
